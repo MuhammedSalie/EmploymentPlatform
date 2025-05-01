85af5cd83d812c5a0fa7810e4047b7bbe9ac096a
Muhammed Salie <muhammed.salie@mmltd.co.za>
2025-05-01 20:46:50 +0200

    add Momento credentials

--- backend/secrets.tf
+++ backend/secrets.tf
@@ -3,3 +3,11 @@ resource "aws_secretsmanager_secret" "credentials" {
   description = "Credentials for job portal"
 }
 
+resource "aws_secretsmanager_secret_version" "credentials" {
+  secret_id     = aws_secretsmanager_secret.credentials.id
+  secret_string = jsonencode({
+    MomentoApiKey = var.MomentoApiKey,
+    MomentoHTTPEndpoint = var.MomentoHTTPEndpoint,
+    MomentoRefreshToken = var.MomentoRefreshToken
+  })
+}
--- backend/variables.tf
+++ backend/variables.tf
@@ -3,3 +3,15 @@ variable "region" {
   description = "The region in which to create/manage resources"
   default     = "us-east-1"
 }
+
+variable "MomentoApiKey" {
+  type        = string
+}
+
+variable "MomentoHTTPEndpoint" {
+  type        = string
+}
+
+variable "MomentoRefreshToken" {
+  type        = string
+}

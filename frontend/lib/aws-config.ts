e1cfc190ab28fe4439244db7f2606cc5c683e820
Muhammed Salie <muhammed.salie@mmltd.co.za>
2025-05-03 00:19:10 +0200

    Remove process.env from aws-config.ts

--- frontend/lib/aws-config.ts
+++ frontend/lib/aws-config.ts
@@ -3,11 +3,11 @@ export const awsConfig = {
   // Cognito
   cognito: {
     //region: process.env.NEXT_PUBLIC_AWS_REGION || "us-east-1",
-    identityPoolId: process.env.NEXT_PUBLIC_COGNITO_IDENTITY_POOL_ID || "",
-    userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID || "",
-    userPoolWebClientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID || "",
+    //identityPoolId: process.env.NEXT_PUBLIC_COGNITO_IDENTITY_POOL_ID || "",
+    //userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID || "",
+    //userPoolWebClientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID || "",
     oauth: {
-      domain: process.env.NEXT_PUBLIC_COGNITO_DOMAIN || "",
+      //domain: process.env.NEXT_PUBLIC_COGNITO_DOMAIN || "",
       scope: ["email", "profile", "openid"],
       redirectSignIn: typeof window !== "undefined" ? `${window.location.origin}/auth/callback` : "",
       redirectSignOut: typeof window !== "undefined" ? `${window.location.origin}/signin` : "",
